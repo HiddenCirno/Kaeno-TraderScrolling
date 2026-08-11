4bb68078ec3920156601e034ce5e6cfabd0d3f8d
HiddenCirno <2301697863@qq.com>
2026-08-11 16:19:54 +0800

    4.1 upgrade

---
+++ TraderScrolling.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+
+namespace TraderScrolling
+{
+    [BepInPlugin("com.kaeno.TraderScrolling", "Kaeno-TraderScrolling", "4.0.0")]
+    public class TraderScrolling : BaseUnityPlugin
+    {
+        internal static ConfigEntry<float> ScrollWheelSpeed { get; set; }
+        internal static ConfigEntry<bool> ReverseWheelDirection { get; set; }
+
+
+        private void Start()
+        {
+            InitConfiguration();
+            new TraderScrollingPatch().Enable();
+            new PlayerCardPatch().Enable();
+		}
+
+        private void InitConfiguration()
+        {
+            ScrollWheelSpeed = Config.Bind("Config", "Scroll Wheel Speed", 30f, "Adjusts scrolling speed when using mousewheel");
+            ReverseWheelDirection = Config.Bind("Config", "Reverse Wheel Direction", false, "Reverses the wheel's input direction to make it more comfortable to use");
+        }
+    }
+}
